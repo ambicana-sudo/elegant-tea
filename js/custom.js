10c48c1aca44d656ff83d4356dadf694a013d202
ambika naga <ambikanaga030@gmail.com>
2019-10-10 11:25:18 +0545

    new changes added

--- js/custom.js
+++ js/custom.js
@@ -125,3 +125,23 @@ $('.collapse').on('shown.bs.collapse', function(){
 	}).on('hidden.bs.collapse', function(){
 	$(this).parent().find(".ti-minus").removeClass("ti-minus").addClass("ti-plus");
 });
+
+// bootstrap select
+$(document).ready(function () {
+    var mySelect = $('#first-disabled2');
+
+    $('#special').on('click', function () {
+      mySelect.find('option:selected').prop('disabled', true);
+      mySelect.selectpicker('refresh');
+    });
+
+    $('#special2').on('click', function () {
+      mySelect.find('option:disabled').prop('disabled', false);
+      mySelect.selectpicker('refresh');
+    });
+
+    $('#basic2').selectpicker({
+      liveSearch: true,
+      maxOptions: 1
+    });
+  });
